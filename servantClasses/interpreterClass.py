593f1acf3772b2fa4e9af1e20478979bed741297
Krish Ramalingam <krishramalingam2008@gmail.com>
2026-01-23 13:25:27 +0000

    made new classes and starting interpreter

---
+++ servantClasses/interpreterClass.py
@@ -0,0 +1,9 @@
+class Interpreter:
+    def __init__(self, sourceCode):
+        self._sourceCode = sourceCode
+        self._lineNumber = 0
+
+    def interpret(self, statements):
+        pass
+    
+    
