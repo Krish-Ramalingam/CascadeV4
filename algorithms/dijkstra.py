ff0560e455fbe0c2d0c483cb03247da6ea05b629
Krish Ramalingam <krishramalingam2008@gmail.com>
2026-01-27 14:59:51 +0000

    some more stuff

--- algorithms/dijkstra.py
+++ algorithms/dijkstra.py
@@ -12,7 +12,33 @@ def dijkstraShuntingYard(tokens):
     def getPrecendence(op):
         return precedence[op] if op in precedence else 0
     
+    def getType(tok):
+        if tok[0] in ["var", "int", "float"]:
+            return "data"
+        elif tok[0] in precedence:
+            return "op"
+        else:
+            return "error"
+        
+    def peek(stack):
+        return stack[len(stack)-1]
+
+        
+    def opStackLogic(op):
+        currprec = getPrecendence(op)
+        topprec = getPrecendence(peek(opstack))
+    
+    
+    
     datstack = []
     opstack = []
-    
+    retstack = []
+
+    # simplified, need to add some extra token analysis
+    for tok in tokens:
+        t = getType(tok)
+        if t == "data":
+            datstack = tok
+        if t == "op":
+            
     
