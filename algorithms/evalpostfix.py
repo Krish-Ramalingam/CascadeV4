3fcc0bca83191f62dd0bcc1d129238ee43e6c3fb
Krish Ramalingam <krishramalingam2008@gmail.com>
2026-01-28 15:29:13 +0000

    starting to make a postexpr

--- algorithms/evalpostfix.py
+++ algorithms/evalpostfix.py
@@ -2,3 +2,17 @@
 evalpostfix.py used to strict evaluate any postfix expressions iteratively rather than recursively
 aim to use if space becomes an issue
 """
+
+postExpr = [...]
+
+def evalPostExpr(expr):
+    
+    def getType(tok):
+        if tok[0] in ["var", "int", "float"]:
+            return "dat"
+        else:
+            return "op"
+    
+    ramStack = []
+    for token in expr:
+        if token[0] in 
