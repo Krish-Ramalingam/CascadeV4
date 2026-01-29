197347861fb7742de3a53257172b28653754eac6
Krish <krishramalingam2008@gmail.com>
2026-01-29 09:04:56 +0000

    Refactor evalPostExpr function for tokenised input
    Refactor evalPostExpr to use tokenised input and implement a queue for data handling.

--- algorithms/evalpostfix.py
+++ algorithms/evalpostfix.py
@@ -5,14 +5,24 @@ aim to use if space becomes an issue
 
 postExpr = [...]
 
-def evalPostExpr(expr):
+def evalPostExpr(tokenisedExpr):
     
     def getType(tok):
         if tok[0] in ["var", "int", "float"]:
             return "dat"
         else:
             return "op"
-    
-    ramStack = []
-    for token in expr:
-        if token[0] in 
+
+    ramQueue = []
+    for token in tokenisedExpr:
+        if getType(token) == "dat":
+            ramQueue.append(token)
+        if getType(token) == "op"
+            firstDat = ramQueue[0]
+            secondDat = ramQueue[0]
+            result = performOperation(firstDat, secondDat, token)
+            ramQueue[1] = result
+            ramQueue = ramQueue[1:]
+
+
+# performOperation(first
